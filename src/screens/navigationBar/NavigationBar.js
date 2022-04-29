caf4ad678529bc96bf77da9918a6f1bda877cede
hayley kim <hayleykim@hayleys-MacBook-Pro.local>
2022-04-29 15:22:04 +1000

    added navigation bar

---
+++ src/screens/navigationBar/NavigationBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './navigation.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {navigationLinks} from '../../helpers/navigationLinks';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function createLinks() {
+    return navigationLinks.map((e, idx) => (
+        <Nav.Link key={idx} href={e.ref}>{e.name}</Nav.Link> 
+    ))
+
+}
+
+function NavigationBar() {
+  return (
+    <div id="home">
+        <Navbar className="navigation__container" expand="md">
+            <Navbar.Brand style={{ marginLeft: '1rem'}} href="#home">Hayley Kim</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse style={{ justifyContent: 'flex-end', marginRight: '1rem', borderColor: 'none'}}>
+                <Nav className="links" style={{ margin: '0 1rem'}}>
+                    {createLinks()}
+                </Nav>  
+            </Navbar.Collapse>
+        
+        </Navbar>
+    </div>
+  )
+}
+
+export default NavigationBar
